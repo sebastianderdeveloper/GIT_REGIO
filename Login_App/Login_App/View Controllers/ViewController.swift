a67c1697621199fe507dd414c66196d1d588c379
Sebastian Steiner <sebastian.steiner@sz-ybbs.ac.at>
2021-12-22 11:49:38 +0100

    Icon hinzugefügt + login auf Startseite verlegt

--- Login_App/Login_App/View Controllers/ViewController.swift
+++ Login_App/Login_App/View Controllers/ViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var signUpButton: UIButton!
     
     @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +30,41 @@ class ViewController: UIViewController {
 
     func setUpElements(){
         Utilities.styleFilledButton(signUpButton)
-        Utilities.styleHollowButton(loginButton)
+       // Utilities.styleHollowButton(loginButton)
+        errorLabel.alpha = 0
+        
+        //Style the Elements
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
+       // Utilities.styleFilledButton(loginButton)
     }
-
+    
+    @IBAction func logginTabbed(_ sender: Any) {
+        print("clicked Login Button")
+        
+        // Validate Text Fields
+        
+        // Create cleaned versions of the text field
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Signing in the user
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                self.errorLabel.text = error!.localizedDescription
+                self.errorLabel.alpha = 1
+                
+            }
+            else {
+                
+                let homeViewController =  self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+                
+                self.view.window?.rootViewController = homeViewController
+                self.view.window?.makeKeyAndVisible()
+                
+            }
+        }
+        
+    }
+    
 }
-
