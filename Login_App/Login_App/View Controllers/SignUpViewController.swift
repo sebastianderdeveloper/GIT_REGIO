ed3d9a4471c43efc716e8e6f76ba1abf3dd1f057
Sebastian Steiner <sebastian.steiner@sz-ybbs.ac.at>
2021-12-26 22:16:46 +0100

    button kategory redesign

--- Login_App/Login_App/View Controllers/SignUpViewController.swift
+++ Login_App/Login_App/View Controllers/SignUpViewController.swift
@@ -152,6 +152,7 @@ class SignUpViewController: UIViewController {
         
         view.window?.rootViewController = homeViewController
         view.window?.makeKeyAndVisible()
+        
     }
  
 }
