c219953bc2c05356010a4a770bab6bccf1712b62
SMOVEs Macbook <bastisteiner03@gmail.com>
2022-02-17 17:38:00 +0100

    qrcode

--- Login_App/Login_App/View Controllers/OrderViewController.swift
+++ Login_App/Login_App/View Controllers/OrderViewController.swift
@@ -47,9 +47,14 @@ class OrderViewController: UIViewController,  UITableViewDelegate, UITableViewDa
         designUI()
         gesamtPreis()
         shapeTableView.reloadData()
+        if let qrImage = generateQrCode("https://www.google.com") {
+                    QRView.image = UIImage(ciImage: qrImage)
+                    let smallLogo = UIImage(named: "Regio black")
+                    smallLogo?.addToCenter(of: QRView)
+                }
         
-        let QRimage = generateQRCode(from: "Hello, world!")
-        self.QRView.image = QRimage
+        /*let QRimage = generateQRCode(from: "Hello, world!")
+        self.QRView.image = QRimage*/
         
     }
     
@@ -224,5 +229,42 @@ class OrderViewController: UIViewController,  UITableViewDelegate, UITableViewDa
         }
         return nil
     }
+    
+    func generateQrCode(_ content: String)  -> CIImage? {
+            let data = content.data(using: String.Encoding.ascii, allowLossyConversion: false)
 
+            let filter = CIFilter(name: "CIQRCodeGenerator")
+
+            filter?.setValue(data, forKey: "inputMessage")
+            filter?.setValue("Q", forKey: "inputCorrectionLevel")
+
+            if let qrCodeImage = (filter?.outputImage){
+                return qrCodeImage
+            }
+
+            return nil
+        }
+
+}
+
+extension UIImage {
+    
+    /// place the imageView inside a container view
+    /// - parameter superView: the containerView that you want to place the Image inside
+    /// - parameter width: width of imageView, if you opt to not give the value, it will take default value of 100
+    /// - parameter height: height of imageView, if you opt to not give the value, it will take default value of 30
+    func addToCenter(of superView: UIView, width: CGFloat = 100, height: CGFloat = 30) {
+        let overlayImageView = UIImageView(image: self)
+        
+        overlayImageView.translatesAutoresizingMaskIntoConstraints = false
+        overlayImageView.contentMode = .scaleAspectFit
+        superView.addSubview(overlayImageView)
+        
+        let centerXConst = NSLayoutConstraint(item: overlayImageView, attribute: .centerX, relatedBy: .equal, toItem: superView, attribute: .centerX, multiplier: 1, constant: 0)
+        let width = NSLayoutConstraint(item: overlayImageView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
+        let height = NSLayoutConstraint(item: overlayImageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 30)
+        let centerYConst = NSLayoutConstraint(item: overlayImageView, attribute: .centerY, relatedBy: .equal, toItem: superView, attribute: .centerY, multiplier: 1, constant: 0)
+        
+        NSLayoutConstraint.activate([width, height, centerXConst, centerYConst])
+    }
 }
